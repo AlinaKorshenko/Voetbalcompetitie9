1622fa4dbbd3ff7429a4c62734c29dd5cc2c47bf
Alina Korshenko <alina.korshenko@student.vives.be>
2026-03-15 22:35:16 +0100

    introductie pagina gemaakt

---
+++ Voetbalcompetitie9Solution/Voetbalcompetitie9/Controllers/InfoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Voetbalcompetitie9.Controllers
+{
+    public class InfoController : Controller
+    {
+        public IActionResult Introductie()
+        {
+            return View();
+        }
+        
+
+        public IActionResult OverOns()
+        {
+            return View();
+        }
+
+    }
+}
