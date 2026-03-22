ea3486edad2c5f85c57fc6c4dcae3538ff6d6cb8
Alina Korshenko <alina.korshenko@student.vives.be>
2026-03-22 13:39:12 +0100

    De “Over ons”-pagina is geïmplementeerd en de footer is aangepast en verbeterd.

--- Voetbalcompetitie9Solution/Voetbalcompetitie9/Program.cs
+++ Voetbalcompetitie9Solution/Voetbalcompetitie9/Program.cs
@@ -37,7 +37,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Info}/{action=Introductie}/{id?}")
     .WithStaticAssets();
 
 app.MapRazorPages()
