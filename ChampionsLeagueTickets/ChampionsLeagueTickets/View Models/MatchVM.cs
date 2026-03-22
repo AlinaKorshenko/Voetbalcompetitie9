f6e23d0fa799124b5b743d4b12e94bfbab503f9f
Alina Korshenko <alina.korshenko@student.vives.be>
2026-03-22 22:15:49 +0100

    Info toevoegen

---
+++ ChampionsLeagueTickets/ChampionsLeagueTickets/View Models/MatchVM.cs
@@ -0,0 +1,12 @@
+﻿namespace Voetbalcompetitie9.ViewModels
+{
+    public class MatchVM
+    {
+
+        public string homeTeamName { get; set; }
+        public int awayTeamName { get; set; }
+        public int stadionName { get; set; }
+        public int date { get; set; }
+
+    }
+}
