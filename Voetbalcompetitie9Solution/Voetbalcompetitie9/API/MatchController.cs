59acdaad45bf8085463bc1f877de79faa8d60ae1
Alina Korshenko <alina.korshenko@student.vives.be>
2026-03-22 20:46:57 +0100

    beginen met lijst van matches

---
+++ Voetbalcompetitie9Solution/Voetbalcompetitie9/API/MatchController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Voetbalcompetitie9.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchController : ControllerBase
+    {
+
+
+
+
+    }
+}
---
+++ Voetbalcompetitie9Solution/Voetbalcompetitie9/Controllers/TicketController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Voetbalcompetitie9.Controllers
+{
+    public class TicketController : Controller
+    {
+        public IActionResult Matches()
+        {
+
+
+            return View();
+        }
+
+
+
+
+
+    }
+}
