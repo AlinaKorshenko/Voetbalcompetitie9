093861ac1ab578dcf5efe173c80e18e88c7ab4c3
Ilona Defevere <ilona.defevere@student.vives.be>
2026-03-31 09:20:26 +0200

    zitplaatsenservice

---
+++ ChampionsLeagueTickets/ChampionsLeagueTickets.Repositories/ZitplaatsenDAO.cs
@@ -0,0 +1,56 @@
+﻿using ChampionsLeagueTickets.Domain.DataDB;
+using ChampionsLeagueTickets.Domain.EntitiesDB;
+using ChampionsLeagueTickets.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChampionsLeagueTickets.Repositories
+{
+    public class ZitplaatsenDAO : IDAO<Zitplaatsen>
+    {
+        private readonly FootballDbContext _dbContext;
+
+        public ZitplaatsenDAO(FootballDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public Task AddAsync(Zitplaatsen entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Zitplaatsen entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Zitplaatsen?> FindByIdAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Zitplaatsen>?> GetAllAsync()
+        {
+            try
+            {
+                return await _dbContext.Zitplaatsens
+                    .ToListAsync();
+            }
+            catch
+            {
+                Console.WriteLine("error in DAO");
+                throw;
+            }
+        }
+
+        public Task UpdateAsync(Zitplaatsen entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChampionsLeagueTickets/ChampionsLeagueTickets.Services/ZitplaatsenService.cs
@@ -0,0 +1,46 @@
+﻿using ChampionsLeagueTickets.Domain.EntitiesDB;
+using ChampionsLeagueTickets.Repositories.Interfaces;
+using ChampionsLeagueTickets.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChampionsLeagueTickets.Services
+{
+    public class ZitplaatsenService : IService<Zitplaatsen>
+    {
+        private IDAO<Zitplaatsen> _zitplaatsenDAO;
+
+        public ZitplaatsenService(IDAO<Zitplaatsen> zitplaatsenDAO)
+        {
+            _zitplaatsenDAO = zitplaatsenDAO;
+        }
+
+        public Task AddAsync(Zitplaatsen entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Zitplaatsen entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Zitplaatsen?> FindByIdAsync(int Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Zitplaatsen>?> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Zitplaatsen entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
