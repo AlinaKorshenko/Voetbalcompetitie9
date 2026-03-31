44810cb392fdc469d39c85a4f2e5f548b49a1926
Ilona Defevere <ilona.defevere@student.vives.be>
2026-03-31 09:10:50 +0200

    vaktypevm toegevoegd in ui laag

---
+++ ChampionsLeagueTickets/ChampionsLeagueTickets/ViewModels/VakTypeVM.cs
@@ -0,0 +1,14 @@
+﻿using ChampionsLeagueTickets.Domain.EntitiesDB;
+
+namespace ChampionsLeagueTickets.ViewModels
+{
+    public class VakTypeVM
+    {
+        public string VakNummer { get; set; } = null!;
+
+        public int Ring { get; set; }
+
+        public string? Omschrijving { get; set; }
+
+    }
+}
