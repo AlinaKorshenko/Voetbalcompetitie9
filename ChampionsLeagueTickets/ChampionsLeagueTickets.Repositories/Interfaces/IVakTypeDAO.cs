da7eafa76527372cd2b890b624ee692a1684ee28
Ilona Defevere <ilona.defevere@student.vives.be>
2026-04-01 14:52:02 +0200

    IVakTypeDAO

---
+++ ChampionsLeagueTickets/ChampionsLeagueTickets.Repositories/Interfaces/IVakTypeDAO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChampionsLeagueTickets.Repositories.Interfaces
+{
+    internal interface IVakTypeDAO
+    {
+    }
+}
