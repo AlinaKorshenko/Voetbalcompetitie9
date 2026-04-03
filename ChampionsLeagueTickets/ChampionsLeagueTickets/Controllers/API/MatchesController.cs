149d9abb2d53710dd2f71289bde69d32848fd5bb
Ilona Defevere <ilona.defevere@student.vives.be>
2026-04-03 09:06:29 +0200

    toevoeging matchescontroller (bevat enkel constructor voorlopig

---
+++ ChampionsLeagueTickets/ChampionsLeagueTickets/Controllers/API/MatchesController.cs
@@ -0,0 +1,23 @@
+﻿using AutoMapper;
+using ChampionsLeagueTickets.Domain.EntitiesDB;
+using ChampionsLeagueTickets.Services;
+using ChampionsLeagueTickets.Services.Interfaces;
+using ChampionsLeagueTickets.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChampionsLeagueTickets.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MatchesController : Controller
+    {
+        private readonly IMapper _mapper;
+        private IService<Match> _matchesService;
+
+        public MatchesController(IMapper mapper, IService<Match> matchesService)
+        {
+            _mapper = mapper;
+            _matchesService = matchesService;
+        }
+    }
+}
