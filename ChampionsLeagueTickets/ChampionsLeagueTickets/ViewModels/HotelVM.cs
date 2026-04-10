1e31dd4d9b93bbb0221b6f10acb89825aedecc82
Ilona Defevere <ilona.defevere@student.vives.be>
2026-04-10 09:53:24 +0200

    toevoeging hotelvm

---
+++ ChampionsLeagueTickets/ChampionsLeagueTickets/ViewModels/HotelVM.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace ChampionsLeagueTickets.ViewModels
+{
+    public class HotelVM
+    {
+        public string? Name { get; set; }
+
+        public string? City { get; set; }
+
+        public string? Country { get; set; }
+
+        public string? Address { get; set; }
+
+        public double? Rating { get; set; }
+
+        public string[]? Amenities { get; set; }
+    }
+}
