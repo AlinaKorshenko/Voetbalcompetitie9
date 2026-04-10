443555e05193398d5390e22541055b33fc30b097
Ilona Defevere <ilona.defevere@student.vives.be>
2026-04-10 09:38:58 +0200

    toevoeging hotelservice + di in program.cs

---
+++ ChampionsLeagueTickets/ChampionsLeagueTickets.Services/HotelService.cs
@@ -0,0 +1,28 @@
+﻿using ChampionsLeagueTickets.Domain.Integrations.HotelsAPI.DTO;
+using ChampionsLeagueTickets.Services.Interfaces;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChampionsLeagueTickets.Services
+{
+    public class HotelService : IHotelService
+    {
+        private IConfiguration _configure;
+        private string? apiBaseUrl;
+
+        public HotelService(IConfiguration configuration)
+        {
+            _configure = configuration;
+            apiBaseUrl = _configure["WebAPIBaseUrl"];
+        }
+
+        public Task<List<HotelItem>?> GetHotelsFromCountryAndCity(string country, string city)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
