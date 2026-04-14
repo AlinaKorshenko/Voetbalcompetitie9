b2cc2d18116a856c792d842175edd6593a765be2
Ilona Defevere <ilona.defevere@student.vives.be>
2026-04-14 21:17:15 +0200

    toevoeging lege abonnementcontroller

---
+++ ChampionsLeagueTickets/ChampionsLeagueTickets/Controllers/AbonnementController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChampionsLeagueTickets.Controllers
+{
+    public class AbonnementController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
