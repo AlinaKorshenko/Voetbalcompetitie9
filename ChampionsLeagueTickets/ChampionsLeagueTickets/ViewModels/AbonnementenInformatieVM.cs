9caea6bd26d35c42f4f74c4b8b2fa9ced2f200ca
Ilona Defevere <ilona.defevere@student.vives.be>
2026-04-17 20:10:22 +0200

    toevoeging iseizoenendao

--- ChampionsLeagueTickets/ChampionsLeagueTickets/ViewModels/AbonnementenInformatieVM.cs
+++ ChampionsLeagueTickets/ChampionsLeagueTickets/ViewModels/AbonnementenInformatieVM.cs
@@ -2,5 +2,14 @@
 {
     public class AbonnementenInformatieVM
     {
+        public string StadionNaam { get; set; }
+
+        public string SeizoenNaam { get; set; }
+
+        public DateOnly StartDatum { get; set; }
+
+        public DateOnly EindDatum { get; set; }
+
+        public decimal Prijs { get; set; }
     }
 }
