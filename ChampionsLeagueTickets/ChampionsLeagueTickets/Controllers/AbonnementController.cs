27b35d6bf565826d17fd6ae252ef01fa0dedc2a7
Ilona Defevere <ilona.defevere@student.vives.be>
2026-04-17 20:31:47 +0200

    toevoeging index.cshtml in abonnement

--- ChampionsLeagueTickets/ChampionsLeagueTickets/Controllers/AbonnementController.cs
+++ ChampionsLeagueTickets/ChampionsLeagueTickets/Controllers/AbonnementController.cs
@@ -1,13 +1,31 @@
-﻿using ChampionsLeagueTickets.Services.Interfaces;
+﻿using AutoMapper;
+using ChampionsLeagueTickets.Domain.EntitiesDB;
+using ChampionsLeagueTickets.Services.Interfaces;
+using ChampionsLeagueTickets.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChampionsLeagueTickets.Controllers
 {
     public class AbonnementController : Controller
     {
-        public IActionResult Index()
+        private readonly IMapper _mapper;
+        private readonly IService<AbonnementenPrijs> _abonnementenPrijsService;
+        private readonly ISeizoenenService _seizoenenService;
+        private readonly IZitplaatsenService _zitplaatsenService;
+
+        public AbonnementController(IMapper mapper, IService<AbonnementenPrijs> abonnementenPrijsService, ISeizoenenService seizoenenService, IZitplaatsenService zitplaatsenService)
         {
-            return View();
+            _mapper = mapper;
+            _abonnementenPrijsService = abonnementenPrijsService;
+            _seizoenenService = seizoenenService;
+            _zitplaatsenService = zitplaatsenService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var abonnementenPrijzen = await _abonnementenPrijsService.GetAllAsync();
+            var vm = _mapper.Map<IEnumerable<AbonnementenInformatieVM>>(abonnementenPrijzen);
+            return View(vm);
         }
     }
 }
