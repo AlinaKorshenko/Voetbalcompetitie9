f204fca4ef638fa03a6eb011a5545ca784aa3046
Ilona Defevere <ilona.defevere@student.vives.be>
2026-05-01 12:59:11 +0200

    installatie packages voor pdf en qr code + aanamaak ipdfservice

---
+++ ChampionsLeagueTickets/ChampionsLeagueTickets.Services/Interfaces/IPdfService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChampionsLeagueTickets.Services.Interfaces
+{
+    public interface IPdfService
+    {
+        public byte[] GenerateTicketPdf(string match, string seat, string ticketId);
+}
